eb88668b085600e7f61ee7de127c962a66bf9fe0
Ed Scheinerman <ers@jhu.edu>
2023-03-10 07:40:22 -0500

    Order illustration

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ where `a` and `b` are relatively prime and `a+b ≤ n`.
 The first rational number produced is `1//1`. Then `1//2` and `2//1`. Then, for `n` equal to `4` and up, we have, in increasing order, 
 the rational numbers of the form `a//b` where `a+b = n` and `gcd(a,b) = 1`. 
 
+This figure illustrates the order in which rational numbers are generated:
+
+![](./order.png)
+
 ## Examples
 
 ```
---
+++ graphic/make_graphic.jl
@@ -0,0 +1,13 @@
+using SimpleDrawing, Plots, RationalGenerators
+
+function make_graphic(max_n::Int = 10)
+    numbers = collect(RationalGenerator(max_n))
+    tops = numerator.(numbers)
+    bots = denominator.(numbers)
+    plot(bots,tops, marker=5, legend=false, aspect_ratio=1)
+    xlabel!("Denominator")
+    ylabel!("Numerator")
+    xaxis!(collect(1:max_n-1))
+    yaxis!(collect(1:max_n))
+    title!("Generator Order")
+end
