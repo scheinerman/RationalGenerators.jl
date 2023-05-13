e56eb4f9d3cbd14ce4ce55d91350c3e26bb6a652
Ed Scheinerman <ers@jhu.edu>
2023-05-13 15:46:13 -0400

    Improve docstrings

--- src/RationalGenerators.jl
+++ src/RationalGenerators.jl
@@ -10,12 +10,19 @@ export RationalGenerator, RatGen
 
 Create an iterator that produces positive rational numbers without repetition. 
 
-`RationalGenerator(n)` creates all rational numbers of the form `a//b`
-where `gcd(a,b) = 1` and `a+b ≤ n`. 
+* `RationalGenerator(n)` creates all rational numbers of the form `a//b` where `gcd(a,b) = 1` and `a+b ≤ n`. 
 
-`RationalGenerator()` creates all rational numbers.
+* `RationalGenerator()` creates all rational numbers. Invoking `RationalGenerator` with a negative argument has the same effect.
 
 Note: `RatGen` may be used an abbrevation in place of `RationalGenerator`.
+
+Example:
+
+```
+julia> collect(RatGen(5))'
+1×9 adjoint(::Vector{Rational{Int64}}) with eltype Rational{Int64}:
+ 1//1  1//2  2//1  1//3  3//1  1//4  2//3  3//2  4//1
+```
 """
 struct RationalGenerator
     max_n::Int
--- src/SmallRatGen.jl
+++ src/SmallRatGen.jl
@@ -6,8 +6,17 @@ export SmallRationalGenerator, SmallRatGen
     SmallRationalGenerator(last_denominator)
 
 Create an iterator that gives rational numbers in the interval `(0,1]` without 
-repetition. If `last_denominator` is specified, include all rationals with 
-that denominator or smaller.
+repetition. 
+
+If `last_denominator` is specified, only produce rationals with 
+that denominator or smaller (a negative argument has the same effect).
+
+Example:
+```
+julia> collect(SmallRatGen(5))'
+1×10 adjoint(::Vector{Rational{Int64}}) with eltype Rational{Int64}:
+ 1//1  1//2  1//3  2//3  1//4  3//4  1//5  2//5  3//5  4//5
+```
 """
 struct SmallRationalGenerator
     last_den::Int
